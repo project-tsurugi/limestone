e04da15a5c6ef8b59f0f8a05f6274468c12c48f3
Nobuhiro Ban <ban@nautilus-technologies.com>
2023-01-17 11:34:51 +0900

    new location prefix calculator (ver.2)

--- src/limestone/logging_helper.h
+++ src/limestone/logging_helper.h
@@ -20,6 +20,8 @@
 
 namespace limestone {
 
+// location prefix ver.1
+
 constexpr std::string_view find_fullname(std::string_view prettyname, std::string_view funcname) {
     // search (funcname + "(")
     size_t fn_pos = 0;  // head of function name
@@ -88,6 +90,170 @@ template<size_t N, size_t M>
 constexpr auto location_prefix(const char (&prettyname)[N], const char (&funcname)[M]) {  // NOLINT
     const std::string_view sv = find_fullname(prettyname, funcname);  // NOLINT
     return location_prefix<std::max(N, M)>(sv);
+}
+
+// location prefix ver.2
+
+constexpr size_t skip_paren(std::string_view a) {
+    const char leftc = a.at(0);
+    const char rightc = leftc == '(' ? ')' : leftc == '<' ? '>' : leftc == '[' ? ']' : throw std::runtime_error("unimplemented paren type");
+    for (size_t i = 1; i < a.length(); i++) {
+        auto c = a.at(i);
+        if (c == '(' || c == '<' || c == '[') {  // nest
+            i += skip_paren(std::string_view{a.data()+i, a.length()-i});
+            continue;
+        }
+        if (c == ')' || c == '>' || c == ']') {
+            if (c == rightc) {
+                // returns offset of right paren
+                return i;
+            }
+            throw std::runtime_error("paren mismatch");
+        }
+        if (a.find("operator", i) == i) {
+            // need special care for operator in template value??
+            // such as "<operator<<, 123>", permit in C++20 or later??
+            throw std::runtime_error("operator in paren not supported");
+        }
+    }
+    throw std::runtime_error("reach end in paren");
+}
+
+template<size_t N>
+constexpr void array_appendstr(std::array<char, N>& buf, size_t &off, const char *str) {
+    for (char c : std::string_view(str)) {
+        buf.at(off++) = c;
+    }
+//std::cout << "DBG: " << str << std::endl;
+}
+
+template<size_t N>
+constexpr auto shrink_prettyname(const char (&prettyname)[N]) {  // NOLINT
+    std::array<char, N> buf{}; // store funcname
+    std::string_view pn{prettyname, N-1};  // NOLINT
+    bool funcname_found = false;  // function-name is found
+    size_t buf_off = 0;
+//try{
+    for (size_t i = 0; i < pn.length(); i++) {
+        auto c = pn.at(i);
+        if (c == ' ') {
+            // skip tailing 'const' and '[...]'
+
+            if (pn.find(" const", i) == i) {
+                if (pn.length() == i + 6 || pn.at(i + 6) == ' ') {
+                    i += 6-1; // skip " const"
+                    continue;
+                }
+            }
+            if (pn.at(i + 1) == '[') {
+                i++;
+                i += skip_paren(std::string_view{pn.data() + i, pn.length() - i});
+                continue;
+            }
+            if (funcname_found) {
+                throw std::runtime_error("unexpected tailing space char");
+            }
+            // currently buffered string was typename, so discard
+            buf_off = 0;  // buf reset
+            continue;
+        }
+        if (pn.find("::<lambda(", i) == i) {
+            // g++ closure, form: "::<lambda(...)>"
+            i += 9;
+            i += skip_paren(std::string_view{pn.data() + i, pn.length() - i});
+            if (pn.at(++i) != '>') {
+                throw std::runtime_error("right '>' error");
+            }
+            i++;
+            array_appendstr(buf, buf_off, ":lambda");
+            if (pn.length() == i) {  // if tail, this is function name
+                funcname_found = true;
+            }
+            continue;
+        }
+        if (pn.find("::(anonymous class)::operator()", i) == i) {
+            // clang closure
+            array_appendstr(buf, buf_off, ":lambda");
+            i += 31-1;
+            continue;
+        }
+        if (c == '(' || c == '<' || c == '[') {
+            i += skip_paren(std::string_view{pn.data() + i, pn.length() - i});
+            if (c == '(') {
+                funcname_found = true;
+            }
+            continue;
+        }
+        if (c == ')' || c == '>' || c == ']') {
+            throw std::runtime_error("paren mismatch");
+        }
+        if (c == ':') {
+            if (pn.at(++i) == ':') {
+                buf.at(buf_off++) = ':';
+//std::cout << "DBG: :" << std::endl;
+            }
+            // else
+            //     TODO: parse error
+            continue;
+        }
+        if (pn.find("operator", i) == i) {
+            i += 8;  // "operator"
+            // this implementation simply drop symbols, i.e. any operatorXX -> "operator"
+            array_appendstr(buf, buf_off, "operator");
+            // g++9: operatorXX(...) const [...]
+            // g++10: operatorXX<...>
+            auto co1 = pn.at(i);  // op 1st char
+            // find the end of operator-name,
+            // skip to '('
+            //         or (for g++10) '<'
+            if (co1 == '(') { // special case: "operator()" contains '('
+                if (pn.at(i+1) == ')') {
+                    i+=2;
+                } else {
+                    throw std::runtime_error("unknown operator(X");
+                }
+            } else if (co1 == '<') {  // (g++10 workaround) special cases: "operator<", "operator<<", "operator<<=", "operator<=>"
+                i++;  // skip '<'
+                if (pn.at(i) == '<') {
+                    i++;  // skip "<<"
+                }
+            }
+            while (pn.at(i) != '(' && pn.at(i) != '<') {
+                i++;
+            }
+            // reached end of operator-name
+            i--;
+        } else {
+            buf.at(buf_off++) = c;
+//std::cout << "DBG: " << c << std::endl;
+        }
+    }
+    if (!funcname_found) { throw std::runtime_error("parse error"); }
+//} catch(const char *p) { std::cout << p << std::endl; }
+
+    buf.at(buf_off++) = 0;
+    return buf;
+}
+
+template<size_t N>
+constexpr size_t shrinked_length(const char (&prettyname)[N]) {  // NOLINT
+    auto buf = shrink_prettyname(prettyname); // store funcname
+    return std::string_view{buf.data()}.size();
+}
+
+template<size_t N>
+constexpr auto location_prefix_v2(const char (&prettyname)[N]) {  // NOLINT
+    auto buf = shrink_prettyname(prettyname); // store funcname
+    //constexpr size_t n = shrinked_length(prettyname) + 1;  // +1: '\0'
+    constexpr size_t n = N;
+    std::array<char, n+3> ret{'/', ':'};
+    size_t i = 0;
+    for (; buf.at(i) != 0; i++) {
+        ret.at(i+2) = buf.at(i);
+    }
+    ret.at(i+2) = ' ';
+    ret.at(i+3) = 0;
+    return ret;
 }
 
 // To force compile-time evaluation of constexpr location_prefix in C++17,
--- test/limestone/utils/logging_helper_test.cpp
+++ test/limestone/utils/logging_helper_test.cpp
@@ -48,6 +48,39 @@ TEST_F(logging_helper_test, location_prefix_constchar) { // NOLINT
     ASSERT_EQ(std::string(location_prefix("foo<myclass>::func(int)", "func").data()), "/:foo:func ");
 }
 
+TEST_F(logging_helper_test, shrink_prettyname) { // NOLINT
+    // check constexpr-ness (if fail, then compile error)
+    constexpr auto a = shrink_prettyname(__PRETTY_FUNCTION__);
+
+#define __CHECK(prettyname, expected) EXPECT_EQ(std::string(shrink_prettyname(prettyname).data()), expected)
+    __CHECK("int foo(int)", "foo");
+    __CHECK("limestone::api::datastore::recover()", "limestone:api:datastore:recover");
+    __CHECK("foo<myclass>::func(int)", "foo:func");
+
+    /// lambda
+    // g++-9 lambda
+    __CHECK("aaaa::bbbb<T, n>::func<long unsigned int, 99>::<lambda(int)>",                               "aaaa:bbbb:func:lambda");
+    // clang++-11 lambda
+    __CHECK("auto aaaa::bbbb<unsigned long, 99>::func(int &, int &)::(anonymous class)::operator()(int)", "aaaa:bbbb:func:lambda");
+
+    /// operator
+    // g++-9
+    __CHECK("long double aa::bb<T, n>::operator<<=(const char*) [with T = long unsigned int; int n = 99]", "aa:bb:operator");
+
+    /// cast
+    // g++-9
+    __CHECK("aa::bb<T, n>::operator std::vector<aa::bb<char* const, -5> >() [with T = long unsigned int; int n = 99]", "aa:bb:operator");
+    __CHECK("aa::bb<T, n>::operator std::vector<aa::bb<char* const, -5> >() [with T = long unsigned int; int n = 99]::<lambda(int)>", "aa:bb:operator:lambda");
+#undef __CHECK
+}
+
+TEST_F(logging_helper_test, DISABLED_shrinked_length) { // NOLINT
+    constexpr size_t n = shrinked_length(__PRETTY_FUNCTION__);
+    std::cout << n << " " << std::string_view(__PRETTY_FUNCTION__).length() << " " << __PRETTY_FUNCTION__;
+    constexpr auto lp = location_prefix_v2(__PRETTY_FUNCTION__);
+    std::cout << " " << lp.data() << lp.size() << std::endl;
+}
+
 // redirect LOG to local-scope lbuf
 #ifdef LOG
 #undef LOG
@@ -79,7 +112,7 @@ public:
             // clang++-11: auto limestone::testing::logging_helper_test_foo2<unsigned long, 99>::foo(int &, int &)::(anonymous class)::operator()(int)
             lbuf.str("");
             LOG_LP(0) << "TEST";
-            //assert(lbuf.str() == ???);
+            //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:foo:lambda TEST");
         };
         lambda1(1);
         std::function<long double(int)> lambda2 = [&lbuf](int u){
@@ -87,7 +120,7 @@ public:
             // clang++-11: auto limestone::testing::logging_helper_test_foo2<unsigned long, 99>::foo(int &, int &)::(anonymous class)::operator()(int)
             lbuf.str("");
             LOG_LP(0) << "TEST";
-            //assert(lbuf.str() == ???);
+            //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:foo:lambda TEST");
             return 0.0;
         };
         lambda2(1);
@@ -108,6 +141,38 @@ public:
         lambda1(1);
         return 0.0;
     }
+    long double operator<<= (const char *p) {
+        std::ostringstream lbuf;
+        //std::cout << "PF:" << __PRETTY_FUNCTION__ << " F:" << __FUNCTION__ << std::endl;
+        // g++-9:      long double limestone::testing::logging_helper_test_foo2<T, n>::operator<<=(const char*) [with T = long unsigned int; int n = 99]
+        LOG_LP(0) << "TEST";
+        //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:operator TEST");
+        auto lambda1 = [&lbuf](int u){
+            //std::cout << "PF:" << __PRETTY_FUNCTION__ << " F:" << __FUNCTION__ << std::endl;
+            // g++-9:      limestone::testing::logging_helper_test_foo2<T, n>::operator<<=(const char*) [with T = long unsigned int; int n = 99]::<lambda(int)>
+            lbuf.str("");
+            LOG_LP(0) << "TEST";
+            //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:operator:lambda TEST");
+        };
+        lambda1(1);
+        return 0.0;
+    }
+    operator std::vector<logging_helper_test_foo2<char * const, -5>> () {
+        std::ostringstream lbuf;
+        //std::cout << "PF:" << __PRETTY_FUNCTION__ << " F:" << __FUNCTION__ << std::endl;
+        // g++-9:      limestone::testing::logging_helper_test_foo2<T, n>::operator std::vector<limestone::testing::logging_helper_test_foo2<char* const, -5> >() [with T = long unsigned int; int n = 99] F:operator std::vector<limestone::testing::logging_helper_test_foo2<char* const, -5> >
+        LOG_LP(0) << "TEST";
+        //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:operator TEST");
+        auto lambda1 = [&lbuf](int u){
+            //std::cout << "PF:" << __PRETTY_FUNCTION__ << " F:" << __FUNCTION__ << std::endl;
+            // g++-9:      limestone::testing::logging_helper_test_foo2<T, n>::operator std::vector<limestone::testing::logging_helper_test_foo2<char* const, -5> >() [with T = long unsigned int; int n = 99]::<lambda(int)>
+            lbuf.str("");
+            LOG_LP(0) << "TEST";
+            //assert(lbuf.str() == "/:limestone:testing:logging_helper_test_foo2:operator:lambda TEST");
+        };
+        lambda1(1);
+        return std::vector<logging_helper_test_foo2<char * const, -5>>{};
+    }
 };
 
 TEST_F(logging_helper_test, assert_in_other_methods) { // NOLINT
@@ -117,6 +182,8 @@ TEST_F(logging_helper_test, assert_in_other_methods) { // NOLINT
     logging_helper_test_foo2<unsigned long, 99> foo2;
     foo2.foo(dummy, dummy);
     foo2("a");
+    foo2 <<= "a";
+    (std::vector<logging_helper_test_foo2<char * const, -5>>)foo2;
 }
 
 }  // namespace limestone::testing
