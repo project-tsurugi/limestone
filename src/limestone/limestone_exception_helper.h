e316186670f2eed57d7c35802fb028b1d63405e3
Shinichi Umegane <sumegane@nifty.com>
2024-09-25 18:03:44 +0900

    cleanup: clang-tidy

--- src/limestone/limestone_exception_helper.h
+++ src/limestone/limestone_exception_helper.h
@@ -35,30 +35,34 @@ inline std::string extract_filename(const std::string& path) {
     return p.filename().string();
 }
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define THROW_LIMESTONE_EXCEPTION(message) \
     { \
-        LOG_LP(ERROR) << message; \
-        throw limestone_exception(std::string(message) + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")"); \
+        LOG_LP(ERROR) << (message); \
+        throw limestone_exception(std::string((message)) + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")"); \
     }
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define THROW_LIMESTONE_IO_EXCEPTION(message, error_code) \
     { \
-        std::string full_message = limestone_io_exception::format_message(message, error_code); \
+        std::string full_message = limestone_io_exception::format_message((message), (error_code)); \
         LOG_LP(ERROR) << full_message; \
-        throw limestone_io_exception(full_message + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")", error_code); \
+        throw limestone_io_exception(full_message + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")", (error_code)); \
     }
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define LOG_AND_THROW_EXCEPTION(message) \
     { \
-        LOG_LP(ERROR) << message; \
-        throw limestone_exception(std::string(message) + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")"); \
+        LOG_LP(ERROR) << (message); \
+        throw limestone_exception(std::string((message)) + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")"); \
     }
 
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define LOG_AND_THROW_IO_EXCEPTION(message, error_code) \
     { \
-        std::string full_message = limestone_io_exception::format_message(message, error_code); \
+        std::string full_message = limestone_io_exception::format_message((message), (error_code)); \
         LOG_LP(ERROR) << full_message; \
-        throw limestone_io_exception(full_message + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")", error_code); \
+        throw limestone_io_exception(full_message + " (at " + extract_filename(__FILE__) + ":" + std::to_string(__LINE__) + ")", (error_code)); \
     }
 
 
