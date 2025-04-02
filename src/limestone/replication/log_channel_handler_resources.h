c438b17b32144d962e6613ab4e5630a89848e86d
Shinichi Umegane <umegane@nautilus-technologies.com>
2025-04-02 10:55:39 +0900

    Add log_channel_handler_resources and its unit tests

---
+++ src/limestone/replication/log_channel_handler_resources.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022-2025 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <limestone/api/log_channel.h>
+#include "handler_resources.h"
+#include "replica_server.h"
+
+namespace limestone::replication {
+
+using namespace limestone::api;
+    
+class log_channel_handler_resources : public handler_resources {
+public:
+    log_channel_handler_resources(
+        socket_io& io,
+        replica_server& server,
+        log_channel& channel)
+        : handler_resources(io), server_(server), channel_(channel) {}
+
+    [[nodiscard]] replica_server& get_server() const { return server_; }
+    [[nodiscard]] log_channel& get_log_channel() const { return channel_; }
+
+private:
+    replica_server& server_;
+    log_channel& channel_;
+};
+    
+
+}  // namespace limestone::replication
---
+++ test/limestone/replication/handler_resources_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <limestone/replication/log_channel_handler_resources.h>
+
+namespace limestone::testing {
+
+using namespace limestone::replication;
+
+class handler_resources_test : public ::testing::Test {
+protected:
+    static constexpr const char* base_location = "/tmp/handler_resources_test";
+
+    void SetUp() override {
+        boost::filesystem::remove_all(base_location);
+        boost::filesystem::create_directories(base_location);
+    }
+
+    void TearDown() override { boost::filesystem::remove_all(base_location); }
+};
+
+TEST_F(handler_resources_test, returns_correct_server_and_channel) {
+    replica_server server{};
+    server.initialize(base_location);
+    auto& ds = server.get_datastore();
+    auto& channel = ds.create_channel(base_location);
+    socket_io io("");
+
+    log_channel_handler_resources resources{io, server, channel};
+
+    EXPECT_EQ(&resources.get_server(), &server);
+    EXPECT_EQ(&resources.get_log_channel(), &channel);
+    EXPECT_EQ(&resources.get_socket_io(), &io); 
+}
+
+TEST(handler_resources_test, returns_correct_socket) {
+    socket_io io("dummy");
+    handler_resources resources{io};
+
+    EXPECT_EQ(&resources.get_socket_io(), &io);
+}
+
+}  // namespace limestone::testing
+
