f82f569d778624757f505058e0694928a8e3c153
Shinichi Umegane <umegane@nautilus-technologies.com>
2025-06-11 10:46:40 +0000

    refactor: handle error in now_nsec function by returning 0 on clock_gettime failure

--- src/limestone/now_nsec.h
+++ src/limestone/now_nsec.h
@@ -5,7 +5,9 @@ namespace limestone::internal {
 
 inline uint64_t now_nsec() {
     struct timespec ts{};
-    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+    if (clock_gettime(CLOCK_MONOTONIC_RAW, &ts) != 0) {
+        return 0; // Retuurn 0 indicating an error
+    }
     return static_cast<uint64_t>(ts.tv_sec) * 1'000'000'000ULL + ts.tv_nsec;
 }
 
