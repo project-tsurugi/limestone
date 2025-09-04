71326ee2b702b266d74b3f7e1faa39a6cc0afe03
Shinichi Umegane <umegane@nautilus-technologies.com>
2025-09-04 14:30:22 +0900

    feat(cli): add tg-grpc-backupd utility and unify CLI sources under cli/
    - Add initial implementation of tg-grpc-backupd as a standalone gRPC backup
      server utility
    - Move dblogutil and replica sources into a shared cli/ subdirectory
    - Update build scripts to reflect new source file locations and targets

---
+++ src/limestone/cli/grpc_server_main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2024-2025 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ #include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "tg-grpc-backupd (gRPC remote backup service) starting..." << std::endl;
+    // TODO: gRPCサーバの初期化・起動処理をここに実装
+    
+    // サーバのメインループや終了処理など
+    
+    return 0;
+}
