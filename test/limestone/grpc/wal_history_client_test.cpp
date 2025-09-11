6a8c1e492089c822baf65a5d7cf540a233686c98
Shinichi Umegane <umegane@nautilus-technologies.com>
2025-09-11 11:42:01 +0000

    test: enable get_wal_history_server_down test to check server unreachable scenario

--- test/limestone/grpc/wal_history_client_test.cpp
+++ test/limestone/grpc/wal_history_client_test.cpp
@@ -85,7 +85,7 @@ TEST_F(wal_history_client_test, get_wal_history_with_entries) {
 // Additional failure-case tests
 namespace limestone::grpc::service::testing {
 
-TEST_F(wal_history_client_test, DISABLED_get_wal_history_server_down) {
+TEST_F(wal_history_client_test, get_wal_history_server_down) {
     // Do NOT start_server(); simulate server unreachable.
     WalHistoryRequest req;
     req.set_version(list_wal_history_message_version);
